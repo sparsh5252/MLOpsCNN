140f57af5454d972e50c2abb3cc6a24e6f4bde62
sparsh5252 <64473455+sparsh5252@users.noreply.github.com>
2020-05-25 11:56:25 -0600

    first

---
+++ model1.py
@@ -0,0 +1,52 @@
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.models import Sequential
+ 
+no_of_epochs = 2
+k_size = 3
+p_size = (2,2)
+
+model = Sequential()
+
+model.add(Convolution2D(filters=256,kernel_size=(k_size,k_size),activation='relu',input_shape=(64, 64, 3)))
+model.add(MaxPooling2D(pool_size= p_size))
+
+
+
+model.add(Convolution2D(filters=256,kernel_size=(k_size,k_size),activation='relu',input_shape=(64, 64, 3)))
+model.add(MaxPooling2D(pool_size= p_size))
+
+model.add(Flatten())
+
+model.add(Dense(units=64, activation='relu'))
+model.add(Dense(units=1, activation='sigmoid'))
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+from keras_preprocessing.image import ImageDataGenerator
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+test_datagen = ImageDataGenerator(rescale=1./255)
+training_set = train_datagen.flow_from_directory(
+        '/content/drive/My Drive/Colab Notebooks/SparshMLOps-Ws/cat_dog_data/train/',
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='binary')
+test_set = test_datagen.flow_from_directory(
+        '/content/drive/My Drive/Colab Notebooks/SparshMLOps-Ws/cat_dog_data/test/',
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='binary')
+
+model.fit(
+        training_set,
+        steps_per_epoch=1000,
+        epochs=no_of_epochs,
+        validation_data=test_set,
+        validation_steps=250)
+
