57ec9ca18ffc2256d78186a68877d727cd4a3d6c
sparsh5252 <64473455+sparsh5252@users.noreply.github.com>
2020-05-25 16:03:43 -0600

    fifth

--- model1.py
+++ model1.py
@@ -1,7 +1,7 @@
 from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
-from keras.layers import Dense
+from keras.layers import Dense , Dropout , BatchNormalization
 from keras.models import Sequential
  
 no_of_epochs = 2
